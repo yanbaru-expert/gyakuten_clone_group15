3750f15d86472cf5f18604d2ed7d889ee44a80a7
miyazakiyu <miyazakiyu@miyazakiyuunoMacBook-Pro.local>
2020-04-17 03:15:22 +0900

    コンフリクトの再修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-   HEAD
   devise_for :users
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  master
   root to: "movies#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
- HEAD
-ActiveRecord::Schema.define(version: 2020_04_15_155527) do
 ActiveRecord::Schema.define(version: 2020_04_16_023647) do
- master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
