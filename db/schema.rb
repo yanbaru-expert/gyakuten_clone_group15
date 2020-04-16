29e0b4468a440825f9500572c31a23cb663368f9
takayamori <morit617@gmail.com>
2020-04-16 21:31:09 +0900

    userテーブルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -62,11 +62,4 @@ ActiveRecord::Schema.define(version: 2020_04_16_023647) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
