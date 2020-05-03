673d797330440fab3b05edcd8b59caac8de19205
takayamori <morit617@gmail.com>
2020-05-03 16:05:27 +0900

    CSV内のコード削除

--- app/models/import_csv.rb
+++ app/models/import_csv.rb
@@ -1,20 +1,2 @@
 class ImportCsv < ApplicationRecord
-
-    def self.import(path)
-       CSV.foreach(path, headers: true) do |row|
-         AwsText.create(
-           title: row["title"],
-           content: row["content"]
-         )
-       end
-    end
-    
-    def self.import(path)
-      CSV.foreach(path, headers: true) do |row|
-        Movie.create(
-          title: row["title"],
-          url: row["url"]
-        )
-      end
-   end
 end
