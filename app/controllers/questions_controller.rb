49e8921b5d2e9a57c2f4b2e24ecf2d41db86d018
miyazakiyu <miyazakiyu@miyazakiyuunoMacBook-Pro.local>
2020-05-05 03:11:17 +0900

    指定箇所とタスク19の修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,21 +1,18 @@
 class QuestionsController < ApplicationController
     def index
       @questions = Question.all.order(id:"DESC")
+      @question = Question.new
     end
 
     def create
       @questions = Question.new(question_params)
-      if @questions.save
-        redirect_to questions_path
-      else
-        render :index
-      end
+      redirect_to questions_path
     end
 
     private
 
     def question_params
-      params.permit(:title, :detail)
+      params.require(:question).permit(:title, :detail)
     end
 
 end
