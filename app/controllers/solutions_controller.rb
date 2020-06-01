b9f59a9458faab3fcaeefcce004f3bedf22e0974
takayamori <morit617@gmail.com>
2020-06-01 16:55:24 +0900

    修正

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,8 +1,13 @@
 class SolutionsController < ApplicationController
     def create
         @question = Question.find(params[:question_id])
-        @question.solutions.create(solution_params)
-        redirect_to question_path(@question)
+        @solution = @question.solutions.new(solution_params)
+        if @solution.save
+            redirect_to question_path(@question), notice: "投稿完了しました。"
+        else
+            flash.now[:alert] = "投稿に失敗しました。"
+            render 'questions/show'
+        end
     end
 
     private
